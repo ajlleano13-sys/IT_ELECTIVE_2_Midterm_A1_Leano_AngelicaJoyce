f93a4b122c96db2f4f1a596eb70f5c5f9a23dc1a
andreapar516-a11y <andreapar516@gmail.com>
2026-07-18 11:12:03 +0800

    Many Changes

--- ConsoleApp1/WebApplication1/Controllers/HomeController.cs
+++ ConsoleApp1/WebApplication1/Controllers/HomeController.cs
@@ -15,6 +15,23 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    public IActionResult Project()
+    { 
+        return View();
+    }
+    public IActionResult Skills()
+    {
+     
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
